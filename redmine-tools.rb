9dd34616f189392c6c494a33099948966c2881ba
vfa-khuongdv <khuongdv@vitalify.asia>
2025-08-21 19:55:45 +0700

    Fix GitHub Actions authentication issue for Homebrew formula updates

--- redmine-tools.rb
+++ redmine-tools.rb
@@ -7,22 +7,22 @@ class RedmineTools < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.0/redmine-tools-darwin-arm64.tar.gz"
-      sha256 "f2fffa2473a4fd1defad5e9962f65202cabe629207e74264b63e262c96c5487b"
+      sha256 "b4f30114254b0182552fa1574ace5b9baac5852b574b2f67a2a9cc0a24d8f1c8"
     end
     if Hardware::CPU.intel?
       url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.0/redmine-tools-darwin-amd64.tar.gz"
-      sha256 "26b5488a908b18e27b8e420beeda70ff73f5d9bc7442a045999e813faaab8a3e"
+      sha256 "cf2aa28a51db1af3583dc6eb74c567b6309f12ea7899abaad6c94776acccf916"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.arch_64_bit?
       url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.0/redmine-tools-linux-arm64.tar.gz"
-      sha256 "815e8c2d3a0aa2f3007a52eb86a17c448435e6461032b24761d464100d80f990"
+      sha256 "3f610b707add0d415a704bcdf72aa39ad956b3fc93f171e65d00d31737dd1ae1"
     end
     if Hardware::CPU.intel?
       url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.0/redmine-tools-linux-amd64.tar.gz"
-      sha256 "6799eaeb30b5851605a9849b5953f97edfae4208be9fc08a6b6ec20d036e6cb3"
+      sha256 "b0210eb9f327805250ef3da4df71641ad6bc47ff55845ded2c8015cb38f988da"
     end
   end
 
