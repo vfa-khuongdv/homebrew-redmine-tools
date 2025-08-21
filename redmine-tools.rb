558c1907d86cb8654f7e428740ceeb561efef906
vfa-khuongdv <khuongdv@vitalify.asia>
2025-08-21 18:22:37 +0700

    Prepare for release v1.0.0

--- redmine-tools.rb
+++ redmine-tools.rb
@@ -1,7 +1,7 @@
 class RedmineTools < Formula
   desc "Command-line tool for working with Redmine projects"
-  homepage "https://github.com/vfa-khuongdv/redmine-tools"
-  url "https://github.com/vfa-khuongdv/redmine-tools/archive/v1.0.0.tar.gz"
+  homepage "https://github.com/vfa-khuongdv/homebrew-redmine-tools"
+  url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/archive/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
