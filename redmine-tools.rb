eee4e5b23bcbf57da374b85c7ae2f4491342c74b
vfa-khuongdv <khuongdv@vitalify.asia>
2025-08-21 19:05:42 +0700

    Fix SHA-256 checksums for v1.0.1 binary release

--- redmine-tools.rb
+++ redmine-tools.rb
@@ -7,22 +7,22 @@ class RedmineTools < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.1/redmine-tools-darwin-arm64.tar.gz"
-      sha256 "f2fffa2473a4fd1defad5e9962f65202cabe629207e74264b63e262c96c5487b"
+      sha256 "c15636f3e633700ca0c945379de235f70802b67a0f028e25abb3891133d38c56"
     end
     if Hardware::CPU.intel?
       url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.1/redmine-tools-darwin-amd64.tar.gz"
-      sha256 "26b5488a908b18e27b8e420beeda70ff73f5d9bc7442a045999e813faaab8a3e"
+      sha256 "90bcad7b0b0cfb17aec8167d978bc7110775cca2dab421cac320fa24170d7436"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.arch_64_bit?
       url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.1/redmine-tools-linux-arm64.tar.gz"
-      sha256 "815e8c2d3a0aa2f3007a52eb86a17c448435e6461032b24761d464100d80f990"
+      sha256 "161592fc9498fddf8951d33acf1718b54e1a111f290c12d7d5f3b08aa0bba60d"
     end
     if Hardware::CPU.intel?
       url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.1/redmine-tools-linux-amd64.tar.gz"
-      sha256 "6799eaeb30b5851605a9849b5953f97edfae4208be9fc08a6b6ec20d036e6cb3"
+      sha256 "2c83d83895dc60a7cf0e72058c51f465f984605261a95c965d87a7fc18485aa9"
     end
   end
 
