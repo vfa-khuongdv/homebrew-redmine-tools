02f443193acb113bc24e7600f01659fd8706b122
vfa-khuongdv <khuongdv@vitalify.asia>
2025-08-21 18:58:23 +0700

    Update Homebrew formula to support binary releases

--- redmine-tools.rb
+++ redmine-tools.rb
@@ -1,14 +1,33 @@
 class RedmineTools < Formula
   desc "Command-line tool for working with Redmine projects"
   homepage "https://github.com/vfa-khuongdv/homebrew-redmine-tools"
-  url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/archive/v1.0.0.tar.gz"
-  sha256 "bd0afde162f74c0c7ef164a34728b42726f399226b1a44bfde82cb534cdec07d"
+  version "1.0.0"
   license "MIT"
 
-  depends_on "go" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.0/redmine-tools-darwin-arm64.tar.gz"
+      sha256 "bd0afde162f74c0c7ef164a34728b42726f399226b1a44bfde82cb534cdec07d"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.0/redmine-tools-darwin-amd64.tar.gz"
+      sha256 "bd0afde162f74c0c7ef164a34728b42726f399226b1a44bfde82cb534cdec07d"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.arch_64_bit?
+      url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.0/redmine-tools-linux-arm64.tar.gz"
+      sha256 "bd0afde162f74c0c7ef164a34728b42726f399226b1a44bfde82cb534cdec07d"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/vfa-khuongdv/homebrew-redmine-tools/releases/download/v1.0.0/redmine-tools-linux-amd64.tar.gz"
+      sha256 "bd0afde162f74c0c7ef164a34728b42726f399226b1a44bfde82cb534cdec07d"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd"
+    bin.install Dir["redmine-tools-*"].first => "redmine-tools"
   end
 
   test do
